a8cc1a684b1f6079b9dda534712fa3cf96261c3e
Animesh <animeshsinha524@gmail.com>
2025-04-04 09:17:13 +0530

    UseStateHooksAssignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,21 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  let [pa , setPara] = useState('')
+
+  function handleClick()
+  {
+    setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
   return (
+    
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id='click' onClick={handleClick}>click</button>
+      <p id ="para">{pa}</p>
+      
+
     </div>
   );
 }
